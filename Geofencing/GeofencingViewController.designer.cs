d7e0e44082da48cd1cdd088c3ea2e9e0fcfe233d
Ahmed Saleh Sobeih <aproram@gmail.com>
2019-06-20 19:55:59 +0300

    initial commit

---
+++ Geofencing/GeofencingViewController.designer.cs
@@ -0,0 +1,36 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Geofencing
+{
+    [Register ("GeofencingViewController")]
+    partial class GeofencingViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button { get; set; }
+
+
+        [Action ("UIButton1_TouchUpInside:")]
+        partial void UIButton1_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("buttonCreateClicked:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void buttonCreateClicked (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (button != null) {
+                button.Dispose ();
+                button = null;
+            }
+        }
+    }
+}
